bb9d3ead16d5376a49e371d795360b42cecc4e9d
Juho Vepsäläinen <bebraw@gmail.com>
2022-07-30 14:18:41 +0300

    feat: Add edge only version

---
+++ ssg-variants/edge-only/functions/posts/[id].ts
@@ -0,0 +1,22 @@
+import postTemplate from "../../../post-template";
+
+// https://github.com/cloudflare/workers-types
+export async function onRequest({ params: { id } }) {
+  const res = await fetch("http://localhost:3000/posts");
+  const posts = await res.json();
+
+  const idAsNumber = parseInt(id, 10);
+  const foundPost = posts.find((p) => p.id === idAsNumber);
+
+  if (!foundPost) {
+    return new Response(`{ "error": "No matching post was found" }`, {
+      status: 500,
+      headers: { "content-type": "application/json" },
+    });
+  }
+
+  return new Response(postTemplate(foundPost), {
+    status: 200,
+    headers: { "content-type": "text/html" },
+  });
+}
