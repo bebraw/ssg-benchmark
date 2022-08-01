c2ab22de12dd6f4860ab2efb90a136eb9276bf52
Juho Vepsäläinen <bebraw@gmail.com>
2022-08-01 14:44:13 +0300

    feat: Add a lorem ipsum generator

--- content.ts
+++ content.ts
@@ -1,13 +1,26 @@
-// import { LoremIpsum } from "lorem-ipsum";
-
 function getPosts(n = 1000) {
-  // const lorem = new LoremIpsum({ random: Math.random });
-
   return Array.from(Array(n).keys()).map((id) => ({
     id,
-    title: "title", // lorem.generateWords(4),
-    content: "content", // lorem.generateParagraphs(20),
+    title: loremIpsum(id, 4),
+    content: loremIpsum(id, 500),
   }));
+}
+
+// https://generator.lorem-ipsum.info/
+const LOREM_IPSUM = `Lorem ipsum dolor sit amet, vix id altera vocibus fastidii. His ut tamquam copiosae, in modus moderatius duo. Nec posse movet labore at, graecis ancillae vel ut, alia illum persequeris has ei. His ad debet virtute, bonorum hendrerit eum ea. Iisque sensibus ex sea. Te novum decore percipitur sed, eos ne populo euismod, at etiam option evertitur per. Ut vis wisi mollis, et quando appellantur vim.
+
+In pro porro labores propriae. Et elit mutat consectetuer mel, his cu summo adipisci. Ex justo latine quo, maluisset evertitur omittantur no vel, paulo petentium torquatos et sit. Vim ea laudem apeirian principes, velit lobortis te vix. Pri nisl reque equidem ea.
+
+Legimus pertinacia ei nec. Ne eam invidunt legendos vituperata. Ornatus dissentias eum te, discere oporteat an nec. Ut quis harum laudem cum. Ad nonumy doming causae has, ea detracto quaestio maiestatis qui, paulo percipit quaerendum eum ea. An esse quando his.
+
+Mel facete vidisse laoreet ea. No vis saepe officiis comprehensam, nec an impedit adolescens concludaturque. Vix omnis reprehendunt ei. Te nec nibh integre, sea an ipsum primis option.
+
+Possit omittam moderatius ne mei, nihil saperet molestiae ne pro. Mei dolore sensibus in, accusamus appellantur duo in, ne eos magna propriae. Tale nobis delicata ne quo, at ius vocibus mnesarchum, nec facer debitis sapientem in. Ex pro dolore civibus eleifend, vivendum delicatissimi est ei. Nostrud volutpat cotidieque ut pro, cibo patrioque dissentias ea eos. Summo movet ut cum, mea legendos moderatius neglegentur eu.`;
+
+function loremIpsum(start: number, amount: number) {
+  return LOREM_IPSUM.split(" ")
+    .slice(start, start + amount)
+    .join(" ");
 }
 
 export { getPosts };
