1f33b69dbc79deb07b744e6ee0f3fef6541b8637
Juho Vepsäläinen <bebraw@gmail.com>
2022-08-15 18:22:06 +0300

    feat: Add 11ty to benchmark

--- benchmark-suites/ssg.ts
+++ benchmark-suites/ssg.ts
@@ -1,18 +1,23 @@
 import b from "benny";
 import runBreezewind from "../ssg/breezewind";
+import run11ty from "../ssg/11ty";
 import runVanilla from "../ssg/vanilla";
 import { range } from "../utils";
 
 const SUITE_NAME = "ssg";
 const BENCHMARK_OUTPUT = "benchmark-output";
 
-const options = range(3, (i) => Math.pow(10, i));
+// TODO: Figure out how to run tests sequentially in different amounts
+// to avoid breaking posts.json since it's generated during tests.
+// Maybe there's a better way to handle data fetching for 11ty.
+const options = [1000]; // range(3, (i) => Math.pow(10, i));
 
 b.suite(
   SUITE_NAME,
   ...options.map((o) =>
     b.add(`breezewind (${o})`, async () => await runBreezewind(o))
   ),
+  ...options.map((o) => b.add(`11ty (${o})`, async () => await run11ty(o))),
   ...options.map((o) =>
     b.add(`vanilla (${o})`, async () => await runVanilla(o))
   ),
