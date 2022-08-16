5212e8f180a3b4c4f8c4aa4c2886ce3bee63566b
Juho Vepsäläinen <bebraw@gmail.com>
2022-08-16 11:15:58 +0300

    chore: Sketch out React template

---
+++ templates/react.tsx
@@ -0,0 +1,58 @@
+import React, { type ReactElement } from "react";
+import type { Post } from "../types";
+
+function postIndexTemplate({
+  base,
+  title,
+  posts,
+}: {
+  base: string;
+  title: string;
+  posts: Post[];
+}) {
+  return baseTemplate({
+    base,
+    title,
+    content: (
+      <ul>
+        {posts.map(({ id, title }) => (
+          <li>
+            <a key={id} href={`./${id}`}>
+              ${title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    ),
+  });
+}
+
+function baseTemplate({
+  base,
+  title,
+  content,
+}: {
+  base: string;
+  title: string;
+  content: ReactElement;
+}) {
+  // TODO: Figure out how to set <!DOCTYPE html> in JSX
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+        <base href={base} />
+        <title>${title}</title>
+      </head>
+      <body>
+        <main>
+          <h1>{title}</h1>
+          <div>{content}</div>
+        </main>
+      </body>
+    </html>
+  );
+}
+
+export { baseTemplate, postIndexTemplate, baseTemplate as postTemplate };
