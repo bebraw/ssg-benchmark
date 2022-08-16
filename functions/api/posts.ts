3b926b7439c55efd5feefd08bd282424fb5b944a
Juho Vepsäläinen <bebraw@gmail.com>
2022-08-16 12:20:10 +0300

    chore: Defer post calculation

--- functions/api/posts.ts
+++ functions/api/posts.ts
@@ -1,10 +1,9 @@
 import { getPosts } from "../../content";
 
 const ONE_HOUR = 60 * 60;
-const POSTS = getPosts();
 
 export function onRequest() {
-  return new Response(JSON.stringify(POSTS, null, 2), {
+  return new Response(JSON.stringify(getPosts(), null, 2), {
     status: 200,
     headers: {
       "content-type": "application/json",
