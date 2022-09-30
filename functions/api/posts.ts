759e9c351bbc848ad25d22cfdb18b134aa3da093
Juho Vepsäläinen <bebraw@gmail.com>
2022-09-30 13:25:06 +0300

    chore: Simplify code

--- functions/api/posts.ts
+++ functions/api/posts.ts
@@ -1,13 +1,11 @@
 import { getPosts } from "../../content";
 
-const ONE_HOUR = 60 * 60;
-
 export function onRequest() {
   return new Response(JSON.stringify(getPosts(), null, 2), {
     status: 200,
     headers: {
       "content-type": "application/json",
-      "cache-control": `max-age=${ONE_HOUR}`,
+      "cache-control": "max-age=3600",
     },
   });
 }
