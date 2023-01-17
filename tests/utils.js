d4b8d853d3edc440d4cceea41be016a5314bcc54
Juho Vepsäläinen <bebraw@gmail.com>
2023-01-17 12:25:35 +0200

    chore: Capture more data

--- tests/utils.js
+++ tests/utils.js
@@ -13,32 +13,44 @@ function getReportsConfiguration(prefix) {
 }
 
 function printCSV() {
-  // TODO: Make this more dynamic and configurable
-  const edgeIndexFCPs = readAudits("cf-edge-index-");
-  const edgePageFCPs = readAudits("cf-edge-page-");
-  const edgeWithIsrIndexFCPs = readAudits("cf-edge-with-isr-index-");
-  const edgeWithIsrPageFCPs = readAudits("cf-edge-with-isr-page-");
-  const vanillaIndexFCPs = readAudits("cf-vanilla-index-");
-  const vanillaPageFCPs = readAudits("cf-vanilla-page-");
-  const netlifyIndexFCPs = readAudits("netlify-vanilla-index-");
-  const netlifyPageFCPs = readAudits("netlify-vanilla-page-");
+  // Check the output JSON files for possible audits
+  const auditTypes = [
+    "first-contentful-paint",
+    "server-response-time",
+    "interactive",
+  ];
 
-  function pickRow(i) {
-    return `${i + 1},${edgeIndexFCPs[i]},${edgePageFCPs[i]},${
-      edgeWithIsrIndexFCPs[i]
-    },${edgeWithIsrPageFCPs[i]},${vanillaIndexFCPs[i]},${vanillaPageFCPs[i]},${
-      netlifyIndexFCPs[i]
-    },${netlifyPageFCPs[i]}`;
-  }
+  auditTypes.forEach((auditType) => {
+    const edgeIndexFCPs = readAudits("cf-edge-index-", auditType);
+    const edgePageFCPs = readAudits("cf-edge-page-", auditType);
+    const edgeWithIsrIndexFCPs = readAudits(
+      "cf-edge-with-isr-index-",
+      auditType
+    );
+    const edgeWithIsrPageFCPs = readAudits("cf-edge-with-isr-page-", auditType);
+    const vanillaIndexFCPs = readAudits("cf-vanilla-index-", auditType);
+    const vanillaPageFCPs = readAudits("cf-vanilla-page-", auditType);
+    const netlifyIndexFCPs = readAudits("netlify-vanilla-index-", auditType);
+    const netlifyPageFCPs = readAudits("netlify-vanilla-page-", auditType);
 
-  // This output should go to main.tex
-  console.log(`a,b,c,d,e,f,g,h,i
+    function pickRow(i) {
+      return `${i + 1},${edgeIndexFCPs[i]},${edgePageFCPs[i]},${
+        edgeWithIsrIndexFCPs[i]
+      },${edgeWithIsrPageFCPs[i]},${vanillaIndexFCPs[i]},${
+        vanillaPageFCPs[i]
+      },${netlifyIndexFCPs[i]},${netlifyPageFCPs[i]}`;
+    }
+
+    console.log(`\nAudit type: ${auditType}`);
+    // This output should go to main.tex
+    console.log(`a,b,c,d,e,f,g,h,i
 ${range(5)
   .map((i) => pickRow(i))
   .join("\n")}`);
+  });
 }
 
-function readAudits(pageType, auditType = "first-contentful-paint") {
+function readAudits(pageType, auditType) {
   const files = glob.sync("benchmark-output/" + pageType + "*-audit.json");
 
   const audits = files.map((f) => fs.readFileSync(f)).map((d) => JSON.parse(d));
